397deda7e0390ed5a3d3bf053fdd951ac3e33ca5
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-08-16 05:41:32 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,13 @@
+const furuts = ['apple', 'banana', 'orange'];
+furuts[2] = "mango"
+console.log(furuts)
+console.log(furuts[1]);
+console.log(furuts.length);
+
+
+const numbers = [12, 13, 13, 15];
+numbers[2] = 14
+console.log(numbers)
+console.log(numbers[3]);
+console.log(numbers.length);
+
