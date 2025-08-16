77b91a9d06df3597e7415105761eba75fbd3a387
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-08-17 05:48:12 +0600

    array task one

---
+++ arrayTaskOne.js
@@ -0,0 +1,6 @@
+const fruits = ['apple', 'orange', 'mango', 'banana']
+console.log(fruits);
+console.log(fruits[2]);
+// change the value of the 2nd index element to jambura
+fruits[2] = 'jambura';
+console.log(fruits);
